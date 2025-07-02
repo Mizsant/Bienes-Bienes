4dbed5661b3974b0a28db8332591e68f72a11178
Misael <105952651+Mizsant@users.noreply.github.com>
2025-07-02 01:54:54 -0400

    go to php

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-    eventListeners();
+    eventListeners(); //menu hamburguesa 
+    darkMode(); //Modo Dark 
 });
 
-function eventListeners() {
+function darkMode() { //Modo Dark
+    
+    //si tu navegador esta por defecto en dark o blanco esto lo pondran en automatico a su estado preferido
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    if (prefiereDarkMode.matches) {
+        document.body.classList.toggle('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    //si el sistema esta por defecto en dark o blanco esto lo pondran en automatico a su estado preferido
+    prefiereDarkMode.addEventListener('change', function () {
+        
+        if (prefiereDarkMode.matches) {
+            document.body.classList.toggle('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    // click en el boton para cambiar de estado. aun no logro que sea permanete
+    const botonDarckMode = document.querySelector('.dark-mode-boton');
+    botonDarckMode.addEventListener('click', function () {
+       document.body.classList.toggle('dark-mode');
+    });
+}
+
+function eventListeners() { //menu hamburguesa 
+
     const mobileMenu = document.querySelector('.mobile-menu')
 
     mobileMenu.addEventListener('click' ,navegacionResponsive)
 }
-//despliegue
+//despliegue menu hamburguesa 
 function navegacionResponsive() {
     //desplegue del menu en resoluciones de tables a celulares
     const navegacion = document.querySelector('.navegacion')
@@ -18,3 +47,4 @@ function navegacionResponsive() {
     }
     // navegacion.classList.toggle('mostrar')
 }
+
